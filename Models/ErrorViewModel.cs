159b14a672a7024ac4a33a91714669868c566bf4
Saaid Faraz <farazsaaid@gmail.com>
2023-07-10 13:22:57 +1200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace uniPlanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
