edbf5fe6248b1de5c72d4a33b66f35cc7e0838c0
Saaid Faraz <farazsaaid@gmail.com>
2023-07-11 13:02:36 +1200

    ProgrammesController Created.

--- Areas/Identity/Data/uniPlannerContext.cs
+++ Areas/Identity/Data/uniPlannerContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using uniPlanner.Areas.Identity.Data;
+using uniPlanner.Models;
 
 namespace uniPlanner.Areas.Identity.Data;
 
@@ -19,4 +20,6 @@ public class uniPlannerContext : IdentityDbContext<uniPlannerUser>
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
     }
+
+    public DbSet<uniPlanner.Models.Programmes> Programmes { get; set; } = default!;
 }
---
+++ Controllers/ProgrammesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using uniPlanner.Areas.Identity.Data;
+using uniPlanner.Models;
+
+namespace uniPlanner.Controllers
+{
+    public class ProgrammesController : Controller
+    {
+        private readonly uniPlannerContext _context;
+
+        public ProgrammesController(uniPlannerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Programmes
+        public async Task<IActionResult> Index()
+        {
+              return _context.Programmes != null ? 
+                          View(await _context.Programmes.ToListAsync()) :
+                          Problem("Entity set 'uniPlannerContext.Programmes'  is null.");
+        }
+
+        // GET: Programmes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Programmes == null)
+            {
+                return NotFound();
+            }
+
+            var programmes = await _context.Programmes
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (programmes == null)
+            {
+                return NotFound();
+            }
+
+            return View(programmes);
+        }
+
+        // GET: Programmes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Programmes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name")] Programmes programmes)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(programmes);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(programmes);
+        }
+
+        // GET: Programmes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Programmes == null)
+            {
+                return NotFound();
+            }
+
+            var programmes = await _context.Programmes.FindAsync(id);
+            if (programmes == null)
+            {
+                return NotFound();
+            }
+            return View(programmes);
+        }
+
+        // POST: Programmes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name")] Programmes programmes)
+        {
+            if (id != programmes.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(programmes);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProgrammesExists(programmes.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(programmes);
+        }
+
+        // GET: Programmes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Programmes == null)
+            {
+                return NotFound();
+            }
+
+            var programmes = await _context.Programmes
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (programmes == null)
+            {
+                return NotFound();
+            }
+
+            return View(programmes);
+        }
+
+        // POST: Programmes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Programmes == null)
+            {
+                return Problem("Entity set 'uniPlannerContext.Programmes'  is null.");
+            }
+            var programmes = await _context.Programmes.FindAsync(id);
+            if (programmes != null)
+            {
+                _context.Programmes.Remove(programmes);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProgrammesExists(int id)
+        {
+          return (_context.Programmes?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
