2d01e72faec7ba96d8baa0bff250dabaaa7360ba
Saaid Faraz <farazsaaid@gmail.com>
2023-07-11 12:25:14 +1200

    Changed names to follow convention.

--- Models/UniversityInfo.cs
+++ Models/UniversityInfo.cs
@@ -5,9 +5,9 @@ namespace uniPlanner.Models
     public class UniversityInfo
     {
         public int ID { get; set; }
-        public string name { get; set; }
-        public string city { get; set; }
-        public string region { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
         public int THErank { get; set; }
         public int QSrank { get; set; }
         public int ARWUrank { get; set; }
