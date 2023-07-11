78d5d1daa4503293856bce34e8661cd0c851f911
Saaid Faraz <farazsaaid@gmail.com>
2023-07-11 12:55:14 +1200

    Created MajorsOffered Model.

---
+++ Models/MajorsOffered.cs
@@ -0,0 +1,10 @@
+﻿namespace uniPlanner.Models
+{
+    public class MajorsOffered
+    {
+        public int ID { get; set; }
+        public ICollection <UniProgrammes> UniProgrammes { get; set; }
+        public string name { get; set; }
+        public string link { get; set; }
+    }
+}
