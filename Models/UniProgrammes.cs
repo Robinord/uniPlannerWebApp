ba3a3b39549fe75520e9570dfba96bdc45e26431
Saaid Faraz <farazsaaid@gmail.com>
2023-07-11 12:55:00 +1200

    Created UniProgrammes Model.

---
+++ Models/UniProgrammes.cs
@@ -0,0 +1,9 @@
+﻿namespace uniPlanner.Models
+{
+    public class UniProgrammes
+    {
+        public int ID { get; set; }
+        public ICollection <UniversityInfo> UniversityID { get; set;}
+        public ICollection <Programmes> ProgrammeID { get; set; }
+    }
+}
