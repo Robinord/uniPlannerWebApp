43fc16908e2d37024f101250d5f825d17428b467
Saaid Faraz <farazsaaid@gmail.com>
2023-07-11 12:18:06 +1200

    Created the UniversityModel

---
+++ Models/UniversityInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace uniPlanner.Models
+{
+    public class UniversityInfo
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string city { get; set; }
+        public string region { get; set; }
+        public int THErank { get; set; }
+        public int QSrank { get; set; }
+        public int ARWUrank { get; set; }
+    }
+}
